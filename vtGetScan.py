764e42cc7308c286404a6a0ad01da835ce374785
Mike R <mikeup28@gmail.com>
2019-02-12 18:03:26 -0800

    Update vtGetScan.py

--- vtGetScan.py
+++ vtGetScan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #Author: Mike R
-#Date: 2/14/2017
-#Purpose: Retrieve the VirusTotal results for a specifc hash. 
+#Date: 2/6/2019
+#Purpose: Retrieve the VirusTotal results for a specifc hash.
 
 #In this instance, the user supplies the hash. This can be automated to accept from a csv or other format
 #!/usr/bin/env python
@@ -10,13 +10,13 @@ import os
 
 fileHash=raw_input("What is the md5/sha1/sha256 file hash to retrieve the VirusTotal Scan for:")
 # To test, please use '7657fcb7d772448a6d8504e4b20168b8'
-
-params = {'apikey': 'API-Key-Here', 'resource': fileHash}
+# Hash not there = 212ce8a9b6453a12206b4679cb947302
+params = {'apikey': '', 'resource': fileHash}
 headers = {
   "Accept-Encoding": "gzip, deflate",
   "User-Agent" : "gzip,  My Python requests library example client or username"
   }
-  
+
 #getting the reply
 response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
   params=params, headers=headers)
@@ -24,25 +24,14 @@ json_response = response.json()
 
 #original JSON Response
 #print json_response
+#for key in json_response.keys():
+#    print key
 
-file1='response.txt'
-f=open(file1, 'w')
-f.write(str(json_response))
-f.close()
-
-#get the values and format correctly
-file1='response.txt'
-f2=open(file1, 'r')
-f3=open('response2.txt', 'w')
-line=f2.read()
-line2=line.replace('},', '\n\n')
-line3=line2.replace('{', '\n')
-line4=line3.replace('u\'', ' ')
-line5=line4.replace('\':',':')
-line6=line5.replace('\',',',')
-line7=line6.replace('positives', '\npositives')
-line8=line7.replace('\'','')
-
-f3.write(line8)
-f2.close()
-f3.close()
+if json_response["response_code"] == 0:
+    print "The file hasn't been scanned in VirusTotal. No results."
+    
+if json_response["response_code"] == 1:
+    print "md5 hash: " + str(json_response["md5"])
+    print "Scan Date: " + str(json_response["scan_date"])
+    print "Positives: " + str(json_response["positives"])
+    print "Total Results: " + str(json_response["total"]) 
